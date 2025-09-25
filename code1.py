8a22e2c6c4f255128096c00037d4035cfb299bf1
anvisgit <anvi.wadhwa2024@vitstudent.ac.in>
2025-09-25 13:50:44 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.metrics import roc_curve, auc
 from wordcloud import WordCloud, STOPWORDS
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
@@ -41,7 +42,7 @@ st.write(
 )
 
 uploaded = st.button("DRUMROLLS PLEASE")
-tab0, tab1, tab2 , tab3= st.tabs(["Analysis","Logistic Regression", "Random Forest", "SVC"])
+tab0, tab1, tab2 , tab3,tab4= st.tabs(["Analysis","Logistic Regression", "Random Forest", "SVC","Ensemble"])
 
 if uploaded:
     df = load()
@@ -59,16 +60,9 @@ if uploaded:
             st.subheader("WordCloud")
             good=" ".join(df[df["sentiment"]==1]["review"])
             bad=" ".join(df[df["sentiment"]==0]["review"])
-            c1,c2=st.columns(2)
-
-            with c1:
-                st.write("positive Reviews")
-                wc_good = WordCloud(width=400, height=300, colormap="Greens", background_color="black").generate(good)
-                st.image(wc_good.to_array())
-            with c2:
-                st.write("Negative reviews")
-                wc_bad = WordCloud(width=400, height=300, colormap="Greens", background_color="black").generate(bad)
-                st.image(wc_bad.to_array())
+            text = " ".join(good + bad)
+            wc=WordCloud(width=400, height=300, colormap="Greens", background_color="black").generate(text)
+            st.image(wc.to_array())
             
         with tab1:
             st.subheader("LOGISTIC REGRESSION")
@@ -102,19 +96,39 @@ if uploaded:
                 ax2.set_title("Learning Curve")
                 ax2.legend()
                 st.pyplot(fig2)
+                st.subheader("Feaure Importance")
 
+            importance = np.abs(model.coef_[0])  # absolute value of coeffnts
+            indices = np.argsort(importance)[-20:]
+                
+            st.bar_chart(pd.Series(importance[indices], index=np.array(vectorizer.get_feature_names_out())[indices]))
+
+            if hasattr(model, "predict_proba"):
+                y_scores = model.predict_proba(testvecx)[:, 1]
+            else:
+                y_scores = model.decision_function(testvecx)
+
+            fpr, tpr, _ = roc_curve(testy, y_scores)
+            roc_auc = auc(fpr, tpr)
+            st.markdown("---")
+            fig, ax = plt.subplots()
+            ax.plot(fpr, tpr, color="green", label=f"AUC = {roc_auc:.2f}")
+            ax.plot([0,1],[0,1],'--',color='gray')
+            ax.set_title("ROC Curve")
+            ax.set_xlabel("False Positive Rate")
+            ax.set_ylabel("True Positive Rate")
+            ax.legend()
+            st.pyplot(fig)
         with tab2:
             st.subheader("RANDOM FOREST")
             model = RandomForestClassifier(n_estimators=20,max_depth=15,max_features='sqrt',n_jobs=-1,random_state=3)
             model.fit(trainvecx, trainy)
             model.fit(trainvecx, trainy)
             ypred = model.predict(testvecx)
-
             st.write("Accuracy:", accuracy_score(testy, ypred))
             st.write("Precision:", precision_score(testy, ypred, average='weighted'))
             st.write("Recall:", recall_score(testy, ypred, average='weighted'))
             st.write("F1 Score:", f1_score(testy, ypred, average='weighted'))
-
             c1,c2=st.columns(2)
             with c1:
                 st.subheader("Confusion Matrix")
@@ -128,7 +142,6 @@ if uploaded:
                 train_sizes, train_scores, test_scores = learning_curve(model, trainvecx, trainy, cv=2, train_sizes=np.linspace(0.1, 1.0, 3), scoring='accuracy')
                 train_mean = np.mean(train_scores, axis=1)
                 test_mean = np.mean(test_scores, axis=1)
-
                 fig2, ax2 = plt.subplots()
                 ax2.plot(train_sizes, train_mean, label='Train Accuracy', color='green')
                 ax2.plot(train_sizes, test_mean, label='Test Accuracy', color='green')
@@ -137,15 +150,30 @@ if uploaded:
                 ax2.set_title("Learning Curve")
                 ax2.legend()
                 st.pyplot(fig2)
+            st.markdown("---")
+            importance = model.feature_importances_
+            indices = np.argsort(importance)[-20:][::-1]  # descending order
+            st.bar_chart(pd.Series(importance[indices], index=np.array(vectorizer.get_feature_names_out())[indices]))
 
+            if hasattr(model, "predict_proba"):
+                y_scores = model.predict_proba(testvecx)[:, 1]
+            else:
+                y_scores = model.decision_function(testvecx)
+            fpr, tpr, _ = roc_curve(testy, y_scores)
+            roc_auc = auc(fpr, tpr)
+            fig, ax = plt.subplots()
+            ax.plot(fpr, tpr, color="green", label=f"AUC = {roc_auc:.2f}")
+            ax.plot([0,1],[0,1],'--',color='gray')
+            ax.set_title("ROC Curve")
+            ax.set_xlabel("False Positive Rate")
+            ax.set_ylabel("True Positive Rate")
+            ax.legend()
+            st.pyplot(fig)
         with tab3:
             st.subheader("SVC")
-            
-    
             model = LinearSVC(C=1.5, max_iter=5000, random_state=42)
             model.fit(trainvecx, trainy)
             ypred = model.predict(testvecx)
-
             st.write("Accuracy :", accuracy_score(testy, ypred))
             st.write("Precision:", precision_score(testy, ypred, average='weighted'))
             st.write("Recall Score:", recall_score(testy, ypred, average='weighted'))
@@ -163,7 +191,6 @@ if uploaded:
                 train_sizes, train_scores, test_scores = learning_curve(model, trainvecx, trainy, cv=2, train_sizes=np.linspace(0.1, 1.0, 3), scoring='accuracy')
                 train_mean = np.mean(train_scores, axis=1)
                 test_mean = np.mean(test_scores, axis=1)
-
                 fig2, ax2 = plt.subplots()
                 ax2.plot(train_sizes, train_mean, label='Train Accuracy', color='green')
                 ax2.plot(train_sizes, test_mean, label='Test Accuracy', color='green')
@@ -172,3 +199,5 @@ if uploaded:
                 ax2.set_title("Learning Curve")
                 ax2.legend()
                 st.pyplot(fig2)
+        with tab4:
+            st.header("Ensemble")
